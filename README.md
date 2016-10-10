34c5cea5ca97a32552b3af7ca5e6cdcbb494daca
majorsl <steve@themajorshome.com>
2016-10-10 09:20:39 -0400

    Added some instructions and an example launchd plist.

---
+++ README.md
@@ -0,0 +1,8 @@
+This script will delete user profiles after a specified number of days and can optionally
+save listed account names.
+
+Useful in an enterprise environment where many different logins take place and are not
+required to be kept.
+
+The example launchd script runs at each machine boot to launch the script. It could be
+modified to run at a timed interval or at each user login via a Login Hook.
--- deleteprofiles.sh
+++ deleteprofiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# version 1.0.6
+# version 1.0.7
 # This script will remove inactive accounts in OS X after a predetermined amount of time
-# (currently set at 5 days.) Adjust mtime to change. Exclude 
+# (currently set at 5 days.) Adjust mtime to change. Exclude users below:
 
 save1="/Users/Shared" #Shared should never be removed.
 save2="/Users/administrator"
@@ -10,9 +10,9 @@ save3="/Users/majorsl"
 USERS=`find /Users -type d -maxdepth 1 -mindepth 1 -not -name "." -mtime +5`
 
 for x in $USERS ; do
-[[ "$x" == "$save1" ]] && continue #skip account 1
-[[ "$x" == "$save2" ]] && continue #skip account 2
-[[ "$x" == "$save3" ]] && continue #skip account 3
+[[ "$x" == "$save1" ]] && continue #save account 1
+[[ "$x" == "$save2" ]] && continue #save account 2
+[[ "$x" == "$save3" ]] && continue #save account 3
 
 echo "Removing account and Home: " $x
 
