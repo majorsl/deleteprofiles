45f8aabe6ac7e28529f23ac7b4d12fe86f1fa4c5
Steve Major <steve@themajorshome.com>
2017-10-09 15:12:44 -0400

    Cleaned variables to use double-quotes. Removed antiquated ticks.

--- deleteprofiles.sh
+++ deleteprofiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# version 1.0.7
+# version 1.0.8
 # This script will remove inactive accounts in OS X after a predetermined amount of time
 # (currently set at 5 days.) Adjust mtime to change. Exclude users below:
 
@@ -7,16 +7,16 @@ save1="/Users/Shared" #Shared should never be removed.
 save2="/Users/administrator"
 save3="/Users/majorsl"
 
-USERS=`find /Users -type d -maxdepth 1 -mindepth 1 -not -name "." -mtime +5`
+USERS=$(find /Users -type d -maxdepth 1 -mindepth 1 -not -name "." -mtime +5)
 
 for x in $USERS ; do
 [[ "$x" == "$save1" ]] && continue #save account 1
 [[ "$x" == "$save2" ]] && continue #save account 2
 [[ "$x" == "$save3" ]] && continue #save account 3
 
-echo "Removing account and Home: " $x
+echo "Removing account and Home: " "$x"
 
-dscl . delete $x
-rm -r $x
+dscl . delete "$x"
+rm -r "$x"
 
 done
